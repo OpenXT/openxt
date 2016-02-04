c36febf208df52c46fd183a0d7c46e9fcc10ad68
Jed <lejosnej@ainfosec.com>
2016-02-04 15:20:31 -0500

    Fix copyrights
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,9 +3,26 @@
 # OpenXT build script.
 # Software license: see accompanying LICENSE file.
 #
-# Contributions by Jean-Edouard Lejosne: Copyright (c) 2016 AIS.
-# Contributions by Christopher Clark: Copyright (c) 2016 BAE Systems
-
+# Copyright (c) 2016 Assured Information Security, Inc.
+# Copyright (c) 2016 BAE Systems
+#
+# Contributions by Jean-Edouard Lejosne
+# Contributions by Christopher Clark
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 # Invocation:
 # Takes a build identifier as an optional argument to the script
 # to enable rerunning this script to continue an interrupted build
--- build-scripts/setup.sh
+++ build-scripts/setup.sh
@@ -4,10 +4,26 @@
 # This script sets up the build host (just installs packages and adds a user),
 # and sets up LXC containers to build OpenXT.
 #
-# Software license: see accompanying LICENSE file.
+# Copyright (c) 2016 Assured Information Security, Inc.
+# Copyright (c) 2016 BAE Systems
 #
-# Contributions by Jean-Edouard Lejosne: Copyright (c) 2016 AIS.
-# Contributions by Christopher Clark: Copyright (c) 2016 BAE Systems.
+# Contributions by Jean-Edouard Lejosne
+# Contributions by Christopher Clark
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
 
 # -- Script configuration settings.
 
