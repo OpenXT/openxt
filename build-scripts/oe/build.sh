c9e850df5df591e450e82ed90acef45326ce013e
Jed <lejosnej@ainfosec.com>
2016-02-04 15:11:32 -0500

    Remove hackery
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- build-scripts/oe/build.sh
+++ build-scripts/oe/build.sh
@@ -11,19 +11,9 @@ ALL_BUILDS_SUBDIR_NAME=%ALL_BUILDS_SUBDIR_NAME%
 mkdir $BUILD_DIR
 cd $BUILD_DIR
 
-# commented out this usual clone line:
-#git clone git://${SUBNET_PREFIX}.${IP_C}.1/${BUILD_USER}/openxt.git
+git clone git://${SUBNET_PREFIX}.${IP_C}.1/${BUILD_USER}/openxt.git
 
-# get the correct openxt.git repo for jethro
-# and merge the branch into master to use it.
-git clone https://github.com/aikidokatech/openxt.git
 cd openxt
-git checkout jethro-merge
-git checkout master
-git config user.email "build@localhost"
-git config user.name "automated build"
-git merge --no-edit jethro-merge
-
 cp example-config .config
 cat >>.config <<EOF
 OPENXT_GIT_MIRROR="${SUBNET_PREFIX}.${IP_C}.1/${BUILD_USER}"
@@ -33,27 +23,6 @@ REPO_DEV_CACERT="/home/build/certs/dev-cacert.pem"
 REPO_DEV_SIGNING_CERT="/home/build/certs/dev-cacert.pem"
 REPO_DEV_SIGNING_KEY="/home/build/certs/dev-cakey.pem"
 EOF
-
-./do_build.sh -s setupoe | tee setupoe.log
-
-# now remove setupoe from STEPS:
-sed -i 's/^STEPS="setupoe,/STEPS="/' ./do_build.sh
-
-# get the right jethro xenclient-oe repo
-# and again merge into master to use it
-cd build/repos/xenclient-oe
-git remote rm origin || echo ok no prior origin
-git remote add origin "https://github.com/aikidokatech/xenclient-oe.git"
-git fetch
-git checkout jethro-merge
-git checkout master
-git config user.email "build@localhost"
-git config user.name "automated build"
-git merge --no-edit jethro-merge
-cd -
-
-# Apply Eric's temporary workaround:
-sed -i 's/^LOCALE_GENERATION_WITH_CROSS-LOCALEDEF = "1"/LOCALE_GENERATION_WITH_CROSS-LOCALEDEF = "0"/' ./build/repos/openembedded-core/meta/recipes-core/glibc/glibc-locale.inc
 
 ./do_build.sh | tee build.log
 
