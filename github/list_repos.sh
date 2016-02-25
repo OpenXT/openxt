c18931caf17f7a37a4498ab0c133f4ddcd9d2e6c
Jed <lejosnej@ainfosec.com>
2016-02-25 15:27:22 -0500

    [github] add a script to list all the OpenXT repos and one to make a release
    Signed-off-by: Jed <lejosnej@ainfosec.com>

---
+++ github/list_repos.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Generate a token there: https://github.com/settings/applications
+TOKEN=$1
+
+# Get the list of OpenXT repos
+repos=`curl -H "Authorization: token $TOKEN" -s "https://api.github.com/users/openxt/repos?per_page=100" | jq '.[].name' | cut -d '"' -f 2`
+
+for i in $repos;
+do
+	echo $i
+done
---
+++ github/release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+# Generate a token there: https://github.com/settings/applications
+
+usage()
+{
+    echo "./release.sh <token> <branch> <tag>"
+    exit $1
+}
+
+[ $# -eq 3 ] || usage 1
+
+TOKEN="$1"
+BRANCH="$2"
+TAG="$3"
+
+# Get the list of OpenXT repos
+repos=`curl -H "Authorization: token $TOKEN" -s "https://api.github.com/users/openxt/repos?per_page=100" | jq '.[].name' | cut -d '"' -f 2`
+
+for i in $repos;
+do
+    # Release!
+    curl -s -X POST "https://api.github.com/repos/openxt/${i}/releases"  \
+    -H "Authorization: token $TOKEN"                                     \
+    -H "Content-Type: application/json"                                  \
+    -d "{\"tag_name\":\"${TAG}\",\"target_commitish\":\"${BRANCH}\",\"name\":\"OpenXT ${TAG}\",\"body\":\"\",\"draft\":false,\"prerelease\":false}"
+done
