4382101e390a8a27ded33aa95dfb2fecb7d1f962
Jed <lejosnej@ainfosec.com>
2016-04-19 10:16:33 -0400

    acms: add skylake ACM
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- do_build.sh
+++ do_build.sh
@@ -895,7 +895,7 @@ do_updates()
         done
 }
 
-ACM_LIST="ivb_snb.acm gm45.acm duali.acm quadi.acm q35.acm q45q43.acm xeon56.acm xeone7.acm hsw.acm bdw.acm"
+ACM_LIST="ivb_snb.acm gm45.acm duali.acm quadi.acm q35.acm q45q43.acm xeon56.acm xeone7.acm hsw.acm bdw.acm skl.acm"
 
 extract_acms()
 {
