6174a50d0347aa66a20ca20e81af9de7d028f620
Eric Chanudet <chanudete@ainfosec.com>
2018-03-21 14:31:30 -0400

    setup.sh: Add libelf-dev to the dependencies.
    CONFIG_UNWINDER_ORC is default on CONFIG_X86_64 and requires it.
    We could use CONFIG_UNWINDER_FRAMEPOINTER instead, but might as well got
    with the default.
    
    Signed-off-by: Eric Chanudet <chanudete@ainfosec.com>

--- build-scripts/oe/setup.sh
+++ build-scripts/oe/setup.sh
@@ -32,7 +32,7 @@ sed -i '/^start)$/a        mkdir -p /dev/shm/network/' /etc/init.d/networking
 PKGS=""
 PKGS="$PKGS openssh-server openssl"
 PKGS="$PKGS sed wget cvs subversion git-core coreutils unzip texi2html texinfo docbook-utils gawk python-pysqlite2 diffstat help2man make gcc build-essential g++ desktop-file-utils chrpath cpio screen bash-completion" # OE main deps
-PKGS="$PKGS guilt iasl quilt bin86 bcc libsdl1.2-dev liburi-perl genisoimage policycoreutils unzip vim sudo rpm curl libncurses5-dev libc6-dev-amd64" # OpenXT-specific deps
+PKGS="$PKGS guilt iasl quilt bin86 bcc libsdl1.2-dev liburi-perl genisoimage policycoreutils unzip vim sudo rpm curl libncurses5-dev libc6-dev-amd64 libelf-dev" # OpenXT-specific deps
 PKGS="$PKGS xorriso fusefat dosfstools" # installer & efiboot.img
 
 apt-get update
